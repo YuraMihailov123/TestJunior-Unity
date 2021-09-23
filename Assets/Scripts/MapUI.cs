258d001574478312317774622bd5b00a3e5c6cf8
YuraMihailov <recon.97@mail.ru>
2021-09-23 16:12:42 +0300

    UPD: mouse control and ui added

---
+++ Assets/Scripts/MapUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject infoWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        infoWindow = transform.GetChild(0).gameObject;
+    }
+
+    public void OnSettingsPressed()
+    {
+        infoWindow.SetActive(true);
+    }
+}
